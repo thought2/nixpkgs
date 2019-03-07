f56bbf2ab75863e19f9aac8360905555959ed116
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-03-07 16:35:21 +0100

    gnome3.gnome-user-share: 3.28.0 -> 3.32.0

--- pkgs/desktops/gnome-3/core/gnome-user-share/default.nix
+++ pkgs/desktops/gnome-3/core/gnome-user-share/default.nix
@@ -5,11 +5,11 @@
 
 stdenv.mkDerivation rec {
   name = "gnome-user-share-${version}";
-  version = "3.28.0";
+  version = "3.32.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-user-share/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
-    sha256 = "04wjnrcdlmyszj582nsda32sgi44nwgrw2ksy11xp17nb09d7m09";
+    sha256 = "0kwpk0l108cqbiz3nzwdq92cm0ipsjhc0m649h5377l16j2wdq03";
   };
 
   passthru = {
@@ -41,8 +41,6 @@ stdenv.mkDerivation rec {
   ];
 
   postInstall = ''
-    mkdir -p $out/share/gsettings-schemas/$name
-    mv $out/share/glib-2.0 $out/share/gsettings-schemas/$name
     glib-compile-schemas "$out/share/gsettings-schemas/$name/glib-2.0/schemas"
   '';
 
